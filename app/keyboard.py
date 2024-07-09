c0cd8bbae8f6c00b76a1554783ed036c946b7c41
Kinoki445 <Kinoki445@gmail.com>
2024-07-09 14:55:24 +0500

    фыв

--- app/keyboard.py
+++ app/keyboard.py
@@ -11,7 +11,9 @@ main.add(InlineKeyboardButton(text = "Добавть заказ", callback_data 
         InlineKeyboardButton(text = "Просмотр заказов", callback_data = "view_orders"),
         InlineKeyboardButton(text = "Удалить заказ", callback_data = "delete_order"),
         InlineKeyboardButton(text = "Изменить заказ", callback_data = "edit_order"),
-        InlineKeyboardButton(text = "Поиск заказов", callback_data = "search_order"),)
+        InlineKeyboardButton(text = "Поиск заказов", callback_data = "search_order"),
+        InlineKeyboardButton(text = "Поменять статус", callback_data = "status_order"))
+main.add(InlineKeyboardButton(text = 'Excel таблица', callback_data = 'exsel'))
 
 sort_orders = InlineKeyboardMarkup(row_width=1)
 # sort_orders.add(InlineKeyboardButton(text = "По месяцам", callback_data = "sort_by_month"),
@@ -26,7 +28,7 @@ sort_orders.add(InlineKeyboardButton(text = "Сортировка по дате"
         InlineKeyboardButton(text = "Выбор заказав в определённом месяце", callback_data = "sort_month_order"),
         InlineKeyboardButton(text = "Поиск по ID", callback_data = "search_id_order"),
         InlineKeyboardButton(text = "Поиск по дате", callback_data = "search_date_order"),
-        
+        InlineKeyboardButton(text = "Свободных мест по дате", callback_data = "search_free_order"),
         InlineKeyboardButton(text = "🔙Назад", callback_data = "close_callback"))
 
 months = InlineKeyboardMarkup(row_width=3)
@@ -57,16 +59,23 @@ async def generate_orders_text_and_markup(orders_page, page, total_pages, is_sor
                 orders_text += "📝 **Информация о заказе**\n"
                 orders_text += "━━━━━━━━━━━━━━━━━━━━\n\n"
                 orders_text += f"📌**ID заказа:** {str(order[0])}\n"
-                orders_text += f"\n🟢**Дата приезда:** {str(order[1])}\n"
-                orders_text += f"🔴**Дата выезда:** {str(order[2])}\n"
-                orders_text += f"\n⏰️**Время приезда:** {str(order[3])}\n"
+                orders_text += f"⚡️**Дата приезда:** {str(order[1])}\n"
+                orders_text += f"⚡️**Дата выезда:** {str(order[2])}\n"
+                orders_text += f"⏰️**Время приезда:** {str(order[3])}\n"
                 orders_text += f"🗺**Маршрут:** {str(order[4])}\n"
-                orders_text += f"\n📈**Количество катамаранов:** {str(order[5])}\n"
-                orders_text += f"\n🤵**Имя:** {str(order[6])}\n"
+                orders_text += f"📈**Количество катамаранов:** {str(order[5])}\n"
+                orders_text += f"🤵**Имя:** {str(order[6])}\n"
                 orders_text += f"📞 **Номер телефона:** {str(order[7])}\n"
-                orders_text += f"\n💰**Цена заказа:** {str(order[8])}\n"
-                orders_text += f"\n📝 **Дополнительные пожелания:** {str(order[9])}\n"
+                orders_text += f"💰**Цена заказа:** {str(order[8])}\n"
+                if order[9] == "" or order[9] == None or order[9] == " " or order[9] == '.': 
+                        orders_text += "\n"
+                else:
+                        orders_text += f"📗 **Дополнительные пожелания:** {str(order[9])}\n"
                 orders_text += "━━━━━━━━━━━━━━━━━━━━\n"
+                if order[10] == True:
+                        orders_text += "✅ **Статус:** Подтверждён!\n\n"
+                else:
+                        orders_text += "❌ **Статус:** Не подтверждён!\n\n"
 
         markup = InlineKeyboardMarkup(row_width=2)
         buttons = []
@@ -81,7 +90,17 @@ async def generate_orders_text_and_markup(orders_page, page, total_pages, is_sor
 
         return orders_text, markup
 
-async def info_text(date_id, date_start, date_end, time_start, route, quantity, customer_name, phone_number, price, additional_wishes):
+async def info_text(date_id, date_start, date_end, time_start, route, quantity, customer_name, phone_number, price, additional_wishes, status):
+        if status == True:
+                last_status = "✅ Статус: Подтверждён!"
+        else:
+                last_status = "❌ Статус: Не подтверждён!"
+
+        if additional_wishes == "" or additional_wishes == None or additional_wishes == " " or additional_wishes == '.':
+                last_wishes = "\n"
+        else:
+                last_wishes = f"\n📗 Дополнительные пожелания: {additional_wishes}\n"
+
         return (f'📝 Информация о заказе\n'
                 f"━━━━━━━━━━━━━━━━━━━━\n\n"
                 f'📌ID заказа: {date_id}\n'
@@ -95,5 +114,6 @@ async def info_text(date_id, date_start, date_end, time_start, route, quantity, 
                 
                 f'💰 Цена заказа: {price}р\n\n'
 
-                f'📝 Дополнительные пожелания: {additional_wishes}\n'
-                f'━━━━━━━━━━━━━━━━━━━━\n\n')
+                f'{last_wishes}\n'
+                f'━━━━━━━━━━━━━━━━━━━━\n'
+                f'{last_status}\n\n')
