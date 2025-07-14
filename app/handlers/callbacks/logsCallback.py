fd2786f13c8b31573af41c0979b29deeeb9d1d05
bubnov-alexander <kinoki445@gmail.com>
2025-07-14 09:51:42 +0500

    Изменил проблему с log file отправкой

--- app/handlers/callbacks/logsCallback.py
+++ app/handlers/callbacks/logsCallback.py
@@ -2,13 +2,19 @@ from aiogram import types
 import os
 from aiogram.types import InputFile
 
-async def send_excel(callback: types.CallbackQuery):
+
+async def send_log_file(callback: types.CallbackQuery):
     logs_file = 'app/storage/logs/bot.log'
 
-    file = InputFile(logs_file)
-    await callback.bot.send_document(callback.from_user.id, file)
+    if not os.path.exists(logs_file):
+        await callback.answer("Файл логов не найден", show_alert=True)
+        return
 
-    os.remove(logs_file)
+    file = InputFile(logs_file)
+    try:
+        await callback.bot.send_document(callback.from_user.id, file)
+    except Exception as e:
+        await callback.answer("Ошибка при отправке файла", show_alert=True)
 
 def register_callback_query_logs(dp):
-    dp.register_callback_query_handler(send_excel, lambda c: c.data == 'logs')
+    dp.register_callback_query_handler(send_log_file, lambda c: c.data == 'logs')
